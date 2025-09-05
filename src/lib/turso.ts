bcb145d26abd3285303b06602fb5db343b4c6874
OgwoGP <digispowerllc@gmail.com>
2025-09-05 07:43:55 +0100

    Updated db conn

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -1,6 +1,7 @@
+// turso.ts
 import { createClient } from "@libsql/client";
 
-const client = createClient({
-  url: "libsql://i3clearid-vercel-icfg-g1gwseeajtube2qkypnwlulr.aws-eu-west-1.turso.io",
-  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTcwMjI0MTYsImlkIjoiZjg1NDg5OTMtNGEyZi00ODNiLWE0NmEtNWYzODI0OTJjNTg1IiwicmlkIjoiNDgzNWM5M2QtMjhjMy00ODk4LWJiZjUtYjhkOWJlMmQzOWY5In0.lbPv0tu6xTCNMZnoeizVetc0LcP6mDHwlsvhkGqhMyWqlAFMxlZQiJur8VrWKiDFzD1Xf-hfk65iNhDMTz8OBQ",
+export const turso = createClient({
+  url: process.env.TURSO_DB_URL!, // same DB URL works
+  authToken: process.env.TURSO_DB_AUTH!,
 });
--- src/lib/tursoEdge.ts
+++ src/lib/tursoEdge.ts
@@ -1,7 +1,7 @@
+// tursoEdge.ts
 import { createClient } from "@libsql/client/web";
 
-const client = createClient({
-  url: "libsql://i3clearid-vercel-icfg-g1gwseeajtube2qkypnwlulr.aws-eu-west-1.turso.io",
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTcwMjI0MTYsImlkIjoiZjg1NDg5OTMtNGEyZi00ODNiLWE0NmEtNWYzODI0OTJjNTg1IiwicmlkIjoiNDgzNWM5M2QtMjhjMy00ODk4LWJiZjUtYjhkOWJlMmQzOWY5In0.lbPv0tu6xTCNMZnoeizVetc0LcP6mDHwlsvhkGqhMyWqlAFMxlZQiJur8VrWKiDFzD1Xf-hfk65iNhDMTz8OBQ",
+export const tursoEdge = createClient({
+  url: process.env.TURSO_DB_URL!,       // move secrets to env
+  authToken: process.env.TURSO_DB_AUTH!,
 });
